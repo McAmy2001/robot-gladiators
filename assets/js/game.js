9dd933db8622face6b55a830225aab86a090fb11
Amy McCabe <mcca0168@umn.edu>
2022-05-14 18:18:25 -0500

    shop input changed

--- assets/js/game.js
+++ assets/js/game.js
@@ -87,7 +87,7 @@ var fight = function(enemy) {
 var startGame = function() {
     //reset player stats
     playerInfo.reset();
-
+    
     for(var i = 0; i < enemyInfo.length; i++)  {
         if (playerInfo.health > 0) {
             // let player know what round they are in
@@ -148,23 +148,21 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
         );
 
+        shopOptionPrompt = parseInt(shopOptionPrompt);
         //use switch to carry out action
         switch (shopOptionPrompt) {
-            case "refill": // new case
-            case "REFILL":
+            case 1:
                 playerInfo.refillHealth();
                 break;
 
-            case "upgrade":
-            case "UPGRADE":
+            case 2:
                 playerInfo.upgradeAttack();
                 break;
 
-            case "leave":
-            case "LEAVE":
+            case 3:
                 window.alert("Leaving the store.");
                 // do nothing, so function will end
                 break;
